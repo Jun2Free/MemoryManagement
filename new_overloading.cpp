d488ab3c0ed0e71bf39aaf89c389e8973a33d50b
Jun2Free <c16618@naver.com>
2022-06-16 13:34:47 -0400

    how to overload 'new' and 'delete'

---
+++ new_overloading.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+
+class MyClass {
+    int _mymember;
+
+public:
+    MyClass()
+    {
+        std::cout << "Constructor is called\n";
+    }
+
+    ~MyClass()
+    {
+        std::cout << "Destructor is called\n";
+    }
+
+    // overload new operator
+    void *operator new[](size_t size)
+    {
+        std::cout << "new: Allocating " << size << " bytes of memory" << std::endl;
+        void *p = malloc(size);
+        // 자동적으로 overload 된다. 실행결과를 보면 오버로드 된 부분이 먼저 출력
+        // 그 다음으로 construct called
+
+        return p;
+    }
+
+    void operator delete[](void *p)
+    {
+        std::cout << "delete: Memory is freed again " << std::endl;
+        free(p);
+    }
+};
+
+int main()
+{
+    MyClass *p = new MyClass[3]();
+    delete[] p;
+}
