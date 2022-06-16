e8f2ac643086a1422b82c96611cd16174907df63
Jun2Free <c16618@naver.com>
2022-06-16 12:13:56 -0400

    Compare 'new' and 'malloc'

---
+++ new.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <iostream>
+
+class MyClass {
+private:
+    int *_number;
+
+public:
+    MyClass()
+    {
+        std::cout << "Allocate memory\n";
+        _number = (int*)malloc(sizeof(int));
+    }
+    ~MyClass()
+    {
+        std::cout << "Delete memory\n";
+        free(_number);
+    }
+    void setNumber(int number) {
+        *_number = number;
+        std::cout << "Number: " << _number << "\n";
+    }
+};
+
+int main() {
+    // allocate memory using malloc
+    /*
+    MyClass *myClass = (MyClass*)malloc(sizeof(MyClass));
+    // malloc 으로 메모리를 할당하는 경우 constructor 가 실행되지 않기 때문에 오류발생
+    myClass->setNumber(42);
+    free(myClass);
+     */
+
+    // allocate memory using new
+    MyClass *myClass = new MyClass();
+    // new 로 메모리 할당을 하는 경우, 자동적으로 constructor 가 실행된다.
+    myClass->setNumber(42);
+    delete myClass;
+
+    return 0;
+}
